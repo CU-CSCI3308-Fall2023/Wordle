50d80aaab41c6c061a1ea6618c0ac757c73c57b4
Fernando Picoral <fernandopicoral@gmail.com>
2023-11-14 15:34:59 -0700

    fix(scoreboard): changed query to only subtract points from incorrect guesses

--- app/src/routers/scoreboard.ts
+++ app/src/routers/scoreboard.ts
@@ -19,7 +19,7 @@ interface ScoreboardQuery {
   position: number;
   username: string;
   /**
-   * The points per game is defined as 7 - number of guesses.
+   * The points per game is defined as 6 - number of incorrect guesses.
    */
   total_points: number;
   avg_points_per_game: number;
@@ -28,21 +28,26 @@ interface ScoreboardQuery {
 
 router.get<never, ScoreboardEntry[]>('/', async (req, res) => {
   const scoreboard = await db.any<ScoreboardQuery>(`
-      WITH user_games AS (SELECT u.id                                  AS user_id,
+      WITH user_games AS (SELECT u.id    AS user_id,
                                  u.username,
-                                 g.id                                  AS game_id,
-                                 COALESCE(7 - COUNT(guesses.guess), 7) AS points
+                                 g.id    AS game_id,
+                                 CASE
+                                     WHEN g.guessed_correctly THEN 7 - COUNT(guesses.guess)
+                                     ELSE 0
+                                     END AS points
                           FROM users u
                                    INNER JOIN games g ON u.id = g.user_id
                                    LEFT JOIN guesses ON g.id = guesses.game_id
-                          GROUP BY u.id, u.username, g.id),
+                          WHERE g.guessed_correctly = TRUE
+                             OR (g.guessed_correctly = FALSE AND guesses.id IS NOT NULL)
+                          GROUP BY u.id, u.username, g.id, g.guessed_correctly),
            user_scores AS (SELECT user_id,
-                                 username,
-                                 SUM(points)    AS total_points,
-                                 AVG(points)    AS avg_points_per_game,
-                                 COUNT(game_id) AS games_played
-                          FROM user_games
-                          GROUP BY user_id, username)
+                                  username,
+                                  SUM(points)    AS total_points,
+                                  AVG(points)    AS avg_points_per_game,
+                                  COUNT(game_id) AS games_played
+                           FROM user_games
+                           GROUP BY user_id, username)
       SELECT ROW_NUMBER() OVER (ORDER BY total_points DESC) AS position,
              username,
              total_points,
