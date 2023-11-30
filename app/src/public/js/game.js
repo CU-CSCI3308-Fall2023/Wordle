ff48054b0ccef6b9e6cae12d43c54fb992488c2d
matayay <matayay2@gmail.com>
2023-11-30 12:44:52 -0700

    some typing functionality implemented

--- app/src/public/js/game.js
+++ app/src/public/js/game.js
@@ -1,18 +1,37 @@
+// Global Variables
 const keyboard = document.getElementById("keyboard");
 const keyboardKeys = keyboard.querySelectorAll("button");
-const currentWord = "";
+let currentWord = "";
+let currentRow = 0;
+
+const renderBoard = () => {
+    const row = document.getElementById(`row-${currentRow}`);
+    const values = row.querySelectorAll("p");
+    const upperCaseWord = currentWord.toUpperCase();
+
+    values.forEach((element, index) => {
+        element.innerHTML = "";
+        if (index < upperCaseWord.length) {
+            element.innerHTML = upperCaseWord[index];
+        }
+    });
+};
 
 keyboardKeys.forEach((element) => {
     element.addEventListener("click", () => {
         if (element.className === "delete") {
             currentWord = currentWord.slice(0, -1);
-            // Call a function that rerenders the letters on the current row of the game board
+            renderBoard();
         } else if (element.className === "enter") {
+            console.log("test");
             // Check if currentWord is 5 characters long
             // Build a javascript object using the current string and make a post request to game.ts
+            // increment currentRow
         } else {
-            currentWord += element.innerHTML;
-            // Call a function that rerenders the letters on the current row of the game board
+            if (currentWord.length < 5) {
+                currentWord += element.innerHTML;
+                renderBoard();
+            }
         }
     });
 });
